9246f541363401027c4886e198c0542e290b0814
raideer <idmania1@gmail.com>
2017-12-17 17:44:11 +0200

    Exporting all lib classes

---
+++ src/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Reader: require('./Reader'),
+    Blueprint: require('./Blueprint'),
+    BlueprintBook: require('./BlueprintBook'),
+    Decoder: require('./Decoder'),
+    Entity: require('./Entity'),
+    Recipe: require('./Recipe')
+};
