a27a194bfb1aed7fa181f6d5609b23cb0a7f191f
raideer <idmania1@gmail.com>
2017-12-17 17:42:19 +0200

    Represents a blueprint

---
+++ src/Blueprint.js
@@ -0,0 +1,50 @@
+const Entity = require('./Entity');
+const { sumObjects } = require('./Utils');
+
+module.exports = class Blueprint {
+    constructor(blueprint, book = null) {
+        this.book = book;
+        this.label = blueprint.label;
+        this.entities = blueprint.entities.map(entity => {
+            return new Entity(entity, this);
+        });
+        this.icons = blueprint.icons;
+        this.version = blueprint.version;
+    }
+
+    setBook(book) {
+        if (!this.book) {
+            this.book = book;
+        } else {
+            throw new Error('Blueprint is already in a BlueprintBook');
+        }
+    }
+
+    rawIngredients(expensive = false) {
+        return this.entities.reduce((accumulator, entity) => {
+            const ingredients = entity.ingredientsRaw(expensive);
+            return ingredients ? sumObjects(accumulator, ingredients) : accumulator;
+        }, {});
+    }
+
+    /**
+     * Returns an object of required item count
+     * @return {Object}
+     */
+    requirements() {
+        return this.entities.reduce((accumulator, entity) => {
+            if (!accumulator[entity.name]) {
+                accumulator[entity.name] = 0;
+            }
+
+            if (!accumulator.total) {
+                accumulator.total = 0;
+            }
+
+            accumulator[entity.name]++;
+            accumulator.total++;
+
+            return accumulator;
+        }, {});
+    }
+};
