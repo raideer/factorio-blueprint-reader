fc2ff90bd96d5f86f4939405697f25db0016cdf7
raideer <idmania1@gmail.com>
2017-12-17 17:45:35 +0200

    just utils

---
+++ src/Utils.js
@@ -0,0 +1,19 @@
+module.exports = {
+    round: (number, precision = 1) => {
+        return Math.round(number * precision) / precision;
+    },
+    
+    sumObjects(a, b) {
+        return Object.keys(a).concat(Object.keys(b))
+        .reduce((obj, k) => {
+            obj[k] = (a[k] || 0) + (b[k] || 0);
+            return obj;
+        }, {});
+    },
+
+    forEachObject(obj, callback, thisarg = null) {
+        for (const i in obj) {
+            callback.call(thisarg, obj[i], i);
+        }
+    }
+};
