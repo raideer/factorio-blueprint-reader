3f2d16fde88cfeb9894356e99b3ca8585d798259
raideer <idmania1@gmail.com>
2017-12-18 01:04:54 +0200

    Added direction types

---
+++ src/types.js
@@ -0,0 +1,12 @@
+module.exports = {
+    direction: {
+        NORTH: 0,
+        NORTHEAST: 1,
+        EAST: 2,
+        SOUTHEAST: 3,
+        SOUTH: 4,
+        SOUTHWEST: 5,
+        WEST: 6,
+        NORTHWEST: 7
+    }
+};
