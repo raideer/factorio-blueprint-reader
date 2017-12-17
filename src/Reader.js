96853b4d9a54b0c5330ca5e8f0106a38ed5c8bb3
raideer <idmania1@gmail.com>
2017-12-17 19:45:59 +0200

    Polished read method

--- src/Reader.js
+++ src/Reader.js
@@ -6,20 +6,22 @@ class Reader {
     read(str, version) {
         const obj = Decoder.decode(str, version);
 
+        let blueprintBook = null;
+
         if ('blueprint' in obj) {
             const blueprint = new Blueprint(obj.blueprint);
-            return new BlueprintBook(blueprint);
+
+            blueprintBook = new BlueprintBook(blueprint);
         } else if ('blueprint_book' in obj) {
-            const book = new BlueprintBook([], obj.blueprint_book);
+            blueprintBook = new BlueprintBook([], obj.blueprint_book);
 
-            for (const blueprint of obj.blueprint_book.blueprints) {
-                book.add(new Blueprint(blueprint.blueprint));
-            }
+            obj.blueprint_book.blueprints.forEach(blueprint => {
+                blueprintBook.add(new Blueprint(blueprint.blueprint));
+            });
 
-            return book;
         }
 
-        return null;
+        return blueprintBook;
     }
 }
 
